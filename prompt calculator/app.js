a285414357dd0d95044b5057753b712e1d8181c1
AbdulRehmanT <talhaabbas79@gmail.com>
2024-02-22 17:17:12 +0500

    Javascript - Prompt Calculator Assignment

---
+++ prompt calculator/app.js
@@ -0,0 +1,27 @@
+// User will add 2 number
+let num1 = prompt("Enter First No.");
+let num2 = prompt("Enter Second No.");
+
+// Allowing String to be shifted into number
+num1 = Number(num1);
+num2 = Number(num2);
+
+// Addition
+let sum = num1 + num2;
+alert("Addition: " + sum);
+console.log("Addition: " + sum);
+
+// Subtraction
+let minus = num1 - num2;
+alert("Subtraction: " + minus);
+console.log("Subtraction: " + minus);
+
+// Multiplication
+let multiply = num1 * num2;
+alert("Multiplication: " + multiply);
+console.log("Multiplication: " + multiply);
+
+// Division
+let divide = num1 / num2;
+alert("Division: " + divide);
+console.log("Division: " + divide);
